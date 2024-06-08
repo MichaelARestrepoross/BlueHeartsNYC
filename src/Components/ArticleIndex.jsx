5cbc1cb42840c0f99ddd0fb2570cc01fc1fc6c6a
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-06-08 15:09:22 -0400

    Updated article Index with map and fetch

--- src/Components/ArticleIndex.jsx
+++ src/Components/ArticleIndex.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+// ArticleIndex.js
+import React, { useEffect, useState } from 'react';
+import ArticleCard from './ArticleCard';
 
-function ArticleIndex() {
+const TopBaseStoriesURL = import.meta.env.VITE_TOPSTORIES_BASE_URL;
+const NYTimesAPI_KEY = import.meta.env.VITE_TIMES_API_KEY;
+
+function ArticleIndex({topic}) {
+  const [articles, setArticles] = useState([]);
+
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch(`${TopBaseStoriesURL}${topic}.json?api-key=${NYTimesAPI_KEY}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setArticles(data.results);
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+
   return (
-    <div>ArticleIndex</div>
-  )
+    <div className="flex">
+        
+      {articles.length > 0 ? (
+        articles.map(article => (
+          <ArticleCard key={article.uri} article={article} />
+        ))
+      ) : (
+        <p>No articles available</p>
+      )}
+    </div>
+  );
 }
 
-export default ArticleIndex
+export default ArticleIndex;
