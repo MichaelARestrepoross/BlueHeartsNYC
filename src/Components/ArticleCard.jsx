00aeb84971be240d9601daaa09ef5a3bae71bd01
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-06-08 14:58:42 -0400

    Updated article card and setup empty tailwind

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
-function ArticleCard() {
+function ArticleCard({ article }) {
   return (
-    <div>ArticleCard</div>
+    <div className="">
+      <img className="" src={article.multimedia[0]?.url} alt={article.multimedia[0]?.caption} />
+      <div className="">
+        <div className="">{article.title}</div>
+        <p className="">
+          {new Date(article.published_date).toLocaleDateString()}
+        </p>
+        <p className="">
+          {article.abstract}
+        </p>
+        <div className="">
+          <a 
+            href={article.url} 
+            className=""
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            Click here
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
