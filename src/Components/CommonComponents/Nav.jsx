eeb675927a3fe63ef6d7ba782057f977f9bddd6c
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-06-08 12:46:22 -0400

    created nav

--- src/Components/CommonComponents/Nav.jsx
+++ src/Components/CommonComponents/Nav.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function Nav() {
-  return (
-    <div>Nav</div>
-  )
+    return (
+        <div>
+          <div className=" bg-blue-600">
+            <div className="p-7 flex flex-row justify-between">
+              <Link to={"/"}>
+                <div className="font-bold text-2xl text-white inline-block" >
+                  <span className="">B</span>lue
+                  <span className="">H</span>earts
+                  <span className="">NYC</span>
+                </div>
+              </Link>
+              <div className="flex items-center">
+                <Link to={"/homepage"}>
+                  <p className="text-4xl">📰</p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
 }
 
 export default Nav
