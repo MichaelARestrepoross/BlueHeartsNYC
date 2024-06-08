6d53c5d7778bc78dc064cc2396ae045841ed9260
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-06-08 15:30:33 -0400

    Updated card with tailwind

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 
 function ArticleCard({ article }) {
   return (
-    <div className="">
-      <img className="" src={article.multimedia[0]?.url} alt={article.multimedia[0]?.caption} />
-      <div className="">
-        <div className="">{article.title}</div>
-        <p className="">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 flex flex-col">
+      <img className="w-full max-h-48 object-cover" src={article.multimedia[0]?.url} alt={article.multimedia[0]?.caption} />
+      <div className="px-6 py-4 flex flex-col flex-grow">
+        <div className="font-bold text-xl mb-2">{article.title}</div>
+        <p className="text-gray-700 text-base mb-2">
           {new Date(article.published_date).toLocaleDateString()}
         </p>
-        <p className="">
+        <p className="text-gray-700 text-base flex-grow">
           {article.abstract}
         </p>
-        <div className="">
+        <div className="flex justify-center items-end mt-auto">
           <a 
             href={article.url} 
-            className=""
+            className="inline-block mt-4 px-6 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
             target="_blank" 
             rel="noopener noreferrer"
           >
-            Click here
+            Click here to go to the article
           </a>
         </div>
       </div>
