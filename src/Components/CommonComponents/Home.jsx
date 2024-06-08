f65c27db2662e7b8ed397c63bc5a587967619988
chris.miranda <christophermiranda@pursuit.org>
2024-06-08 16:04:37 -0400

    added navigate to home.jsx, created home.css

--- src/Components/CommonComponents/Home.jsx
+++ src/Components/CommonComponents/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import "./Home.css"
+
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <div>
     <h1>Images</h1>
