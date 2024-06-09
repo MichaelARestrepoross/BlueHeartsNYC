0ae597cd79ba2842fa8752f31fe3ec3e709774aa
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-06-09 12:16:35 -0400

    Updated nav

--- src/Components/CommonComponents/Footer.jsx
+++ src/Components/CommonComponents/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer>
       {/* bg-gradient-to-r from-dark-orange  to-orange shadow-lg shadow-slate-400" */}
-      <div className="bg-gradient-to-r to-duke-blue to-byz-blue from-powder-blue shadow-lg shadow-slate-400 fixed bottom-0 left-0 right-0">
+      <div className="bg-gradient-to-r to-duke-blue to-byz-blue from-powder-blue shadow-lg shadow-slate-400 bottom-0 left-0 right-0">
         <div className="flex flex-row justify-between">
           <Link to={"/"}>
             <div className="p-5 lg:pr-16 font-bold text-2xl text-white inline-block pt-3 pr-10" style={{ fontFamily: 'Orbitron, sans-serif', fontStyle: 'normal' }}>
