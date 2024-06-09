4d843641e05dc521a75dfde59d02e6892c40573d
chris.miranda <christophermiranda@pursuit.org>
2024-06-09 15:09:14 -0400

    added css to pillars

--- src/Components/CommonComponents/Home.jsx
+++ src/Components/CommonComponents/Home.jsx
@@ -47,7 +47,6 @@ function Home() {
         </div>
       </div>
 
-      <h1>Images</h1>
         <section className='four-pillars-frame'> 
           <img src="https://res.cloudinary.com/drsj2l9it/image/upload/v1717704610/A6947A2B-9D6E-40A3-83D1-F2195AF0CA83_4_5005_c_x8ccti.jpg" alt="Move Naturally Image" onClick={() => navigate('/moveNaturally')}/>
           <img src="https://res.cloudinary.com/drsj2l9it/image/upload/v1717704610/575FD410-7711-49CE-9BB6-98976EF76957_4_5005_c_hg20wu.jpg" alt="Outlook Image" onClick={() => navigate('/rightoutlook')}/>
