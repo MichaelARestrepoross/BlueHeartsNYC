a7ace1b0ee1a883bfd39c5d638d7e810418218cf
chris.miranda <christophermiranda@pursuit.org>
2024-06-09 15:23:06 -0400

    finished rough css

--- src/Components/EatWisely.jsx
+++ src/Components/EatWisely.jsx
@@ -6,11 +6,11 @@ function EatWisely() {
   return (
     <div className='pillar-wrapper'>
       <div className='pillar-descrip'>
-      <h1>Eat Wisely</h1>
+        <h1>Eat Wisely</h1>
 
-      <h3>Inspired by the recipes of the best-selling Blue Zones Kitchen cookbooks, Dan Buettner leads you through a step-by-step cooking course full of plant-forward recipes, including convenient store locators for your nearby healthiest groceries to shop items. Don't feel like cooking? Receive the nutrition info and tips for longevity from our meal and food coaches to make wiser choices when eating out.</h3>
+        <h3>Inspired by the recipes of the best-selling Blue Zones Kitchen cookbooks, Dan Buettner leads you through a step-by-step cooking course full of plant-forward recipes, including convenient store locators for your nearby healthiest groceries to shop items. Don't feel like cooking? Receive the nutrition info and tips for longevity from our meal and food coaches to make wiser choices when eating out.</h3>
 
-    </div>
+      </div>
     <img 
         src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1717692433/BlueHeartsNYC-EatWisely_ut3vlf.jpg" 
         alt="Blue Hearts NYC Slide"
--- src/Components/OutLook.jsx
+++ src/Components/OutLook.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
 import ArticleIndex from './ArticleIndex'
+import './Pillars.css'
 
 function Outlook() {
   return (
-    <>
-    <div>Outlook</div>
+    <div className='pillar-wrapper'>
+      <div className='pillar-descrip'>
 
-    <h3>People in the Blue Zones have a sense of purpose, a reason to wake up in the morning, which can add up to seven years of extra life expectancy. They also take time to downshift, pray, meditate, or nap.</h3>
+        <h1>Outlook</h1>
 
-    <h2>Our Goal: Cultivate a sense of purpose and integrate practices that reduce stress and promote mental well-being.</h2>
-
-    <p>Make New York City: Encourage New Yorkers to find and nurture their sense of purpose, whether through career, hobbies, or community service. Promote daily practices such as meditation, mindfulness, or simply taking moments to relax and decompress, contributing to overall mental health and longevity. </p>
+        <h3>People in the Blue Zones have a sense of purpose, a reason to wake up in the morning, which can add up to seven years of extra life expectancy. They also take time to downshift, pray, meditate, or nap.</h3>
+      </div>
     <img 
         src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1717784086/BlueHeartsNYC-GIFOutlook_yvntbi.gif" 
         alt="Blue Hearts NYC Slide"
         style={{ maxWidth: '100%', height: 'auto' }}
       />
+      <br />
+      <section className='goal-wrapper'>
+
+    <h2>Our Goal </h2> 
+    <p>
+    Cultivate a sense of purpose and integrate practices that reduce stress and promote mental well-being.
+    </p> 
+    <br />
+
+    <p>Make New York City: Encourage New Yorkers to find and nurture their sense of purpose, whether through career, hobbies, or community service. Promote daily practices such as meditation, mindfulness, or simply taking moments to relax and decompress, contributing to overall mental health and longevity. </p>
+      </section>
+
     <ArticleIndex topic = {"books/review"}/>
-    </>
+    </div>
   )
 }
 
